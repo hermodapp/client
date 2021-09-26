19f6e34dbae5d01522512f0905b3d90b743592fe
Do Thien An Duong <root@DESKTOP-EOLPRDI.localdomain>
2021-09-26 18:20:30 -0500

    Add types for Tailwind and React-Router

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
