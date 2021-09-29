826c06b8090b492e9d66caec9d96b8ae3e1a5c30
Do Thien An Duong <root@DESKTOP-EOLPRDI.localdomain>
2021-09-28 22:24:44 -0500

    Change style for Landing page, login page and register page

--- src/components/LandingPage/LandingPage.tsx
+++ src/components/LandingPage/LandingPage.tsx
@@ -1,92 +1,139 @@
-import React, {Component, useState, useRef, useEffect} from "react";
-import { NavLink, Link} from "react-router-dom";
-import "../../index.css"
+import React, { Component, useState, useRef, useEffect } from "react";
+import { NavLink, Link } from "react-router-dom";
+import "../../index.css";
 
-export default class LandingPage extends React.Component{
+export default class LandingPage extends React.Component {
   render() {
-    return ( 
-    <>
-      <section className="text-gray-700 body-font">
-        <div className="container mx-auto flex px-5 py-24 md:flex-row flex-col items-center">
-          <div className="flex-grow lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
-            <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900">Who We Are
-            </h1>
-            <p className="text-nord3 font-medium text-justify">
-              Hermod is an automated customer interaction tool which streamlines
-              communication between businesses and their customers. Customers
-              simply scan a QR code printed in a public place to load a Hermod
-              webform, which allows them to rapidly and seamlessly interact with
-              the company with knowledge of the customer’s location (i.e. table
-              at a restaurant, hotel room number, bathroom stall number) without
-              disclosing personal information. After the webform is submitted,
-              the application will dispatch an automated message on their behalf
-              to the company. This message could be performed using a Twilio
-              text-to-speech message, an SMS text message, email, or a push
-              notification.
-            </p>
-            <div className="flex justify-center">
-              <Link to="./Services">
-                <button className="mt-6 inline-flex text-nord1 bg-nord7 border-0 py-2 px-6 focus:outline-none hover:bg-nord1 hover:text-nord7 rounded text-lg">Our Services</button>
-              </Link>
-              <Link to="/">
-                <button className="mt-6 ml-4 inline-flex text-nord1 border-0 py-2 px-6 focus:outline-none hover:bg-nord8 rounded text-lg">Start Here</button>
-              </Link>
-            </div>
-          </div>
-        </div>
-      </section>
-      <section className="text-gray-700 body-font border-t border-gray-200">
-        <div className="container px-5 py-24 mx-auto">
-          <div className="flex flex-col text-center w-full mb-20">
-            <h1 className="sm:text-3xl text-2xl font-medium title-font mb-4 text-gray-900">Our Team</h1>
-            <p className="lg:w-2/3 mx-auto leading-relaxed text-base">We are group of seniors in Computer Science at The University of Alabama.</p>
-          </div>
-          <div className="flex flex-wrap -m-2">
-            <div className="p-2 lg:w-1/2 md:w-1/2 w-full">
-              <div className="h-full flex items-center border-gray-200 border p-4 rounded-lg">
-                <img alt="team" className="w-16 h-16 bg-gray-100 object-cover object-center flex-shrink-0 rounded-full mr-4" src="https://dummyimage.com/80x80/edf2f7/a5afbd"/>
-                <div className="flex-grow">
-                  <h2 className="text-gray-900 title-font font-medium">Russell Weas</h2>
-                  <p className="text-gray-500">Backend Developer</p>
-                </div>
-              </div>
-            </div>
-            <div className="p-2 lg:w-1/2 md:w-1/2 w-full">
-              <div className="h-full flex items-center border-gray-200 border p-4 rounded-lg">
-                <img alt="team" className="w-16 h-16 bg-gray-100 object-cover object-center flex-shrink-0 rounded-full mr-4" src="https://dummyimage.com/84x84/edf2f7/a5afbd"/>
-                <div className="flex-grow">
-                  <h2 className="text-gray-900 title-font font-medium">Jonathan Pence</h2>
-                  <p className="text-gray-500">Backend Developer</p>
-                </div>
-              </div>
-            </div>
-            <div className="p-2 lg:w-1/2 md:w-1/2 w-full">
-              <div className="h-full flex items-center border-gray-200 border p-4 rounded-lg">
-                <img alt="team" className="w-16 h-16 bg-gray-100 object-cover object-center flex-shrink-0 rounded-full mr-4" src="https://dummyimage.com/88x88/edf2f7/a5afbd"/>
-                <div className="flex-grow">
-                  <h2 className="text-gray-900 title-font font-medium">Thien An Duong Do</h2>
-                  <p className="text-gray-500">Frontend Developer</p>
-                </div>
-              </div>
-            </div>
-            <div className="p-2 lg:w-1/2 md:w-1/2 w-full">
-              <div className="h-full flex items-center border-gray-200 border p-4 rounded-lg">
-                <img alt="team" className="w-16 h-16 bg-gray-100 object-cover object-center flex-shrink-0 rounded-full mr-4" src="https://dummyimage.com/90x90/edf2f7/a5afbd"/>
-                <div className="flex-grow">
-                  <h2 className="text-gray-900 title-font font-medium">Sanghyun Simon Jeon</h2>
-                  <p className="text-gray-500">Frontend Developer</p>
-                </div>
+    return (
+      <>
+        <section className="text-gray-700 body-font">
+          <div className="container mx-auto flex px-5 py-24 md:flex-row flex-col items-center">
+            <div className="flex-grow lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
+              <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900">
+                Who We Are
+              </h1>
+              <p className="text-nord3 font-medium text-justify">
+                Hermod is an automated customer interaction tool which
+                streamlines communication between businesses and their
+                customers. Customers simply scan a QR code printed in a public
+                place to load a Hermod webform, which allows them to rapidly and
+                seamlessly interact with the company with knowledge of the
+                customer’s location (i.e. table at a restaurant, hotel room
+                number, bathroom stall number) without disclosing personal
+                information. After the webform is submitted, the application
+                will dispatch an automated message on their behalf to the
+                company. This message could be performed using a Twilio
+                text-to-speech message, an SMS text message, email, or a push
+                notification.
+              </p>
+              <div className="flex justify-center">
+                <Link to="./Services">
+                  <button className="mt-6 inline-flex text-nord1 bg-nord7 border-0 py-2 px-6 focus:outline-none hover:bg-nord8 hover:text-nord3 rounded text-lg">
+                    Our Services
+                  </button>
+                </Link>
+                <Link to="/">
+                  <button className="mt-6 ml-4 inline-flex text-nord1 border border-nord3 py-2 px-6 hover:border-nord8 hover:text-nord8 rounded text-lg">
+                    Start Here
+                  </button>
+                </Link>
               </div>
             </div>
           </div>
-          <NavLink to="/About" className="mt-3 text-indigo-500 inline-flex items-center">Learn More
-            <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" className="w-4 h-4 ml-2" viewBox="0 0 24 24">
-              <path d="M5 12h14M12 5l7 7-7 7"></path>
-            </svg>
-          </NavLink>
-        </div>
-      </section>
-      {/* <section className="text-gray-700 body-font border-t border-gray-200">
+        </section>
+        <section className="text-gray-700 body-font border-t border-gray-200">
+          <div className="container px-5 py-24 mx-auto">
+            <div className="flex flex-col text-center w-full mb-10">
+              <h1 className="sm:text-3xl text-2xl font-medium title-font mb-4 text-gray-900">
+                Our Team
+              </h1>
+              <p className="lg:w-2/3 mx-auto leading-relaxed text-base">
+                We are a group of seniors majoring in Computer Science at The
+                University of Alabama.
+              </p>
+            </div>
+            <div className="flex flex-wrap -m-2">
+              <div className="p-2 lg:w-1/2 md:w-1/2 w-full">
+                <div className="h-full flex items-center border-gray-200 border p-4 rounded-lg">
+                  <img
+                    alt="team"
+                    className="w-16 h-16 bg-gray-100 object-cover object-center flex-shrink-0 rounded-full mr-4"
+                    src="https://dummyimage.com/80x80/edf2f7/a5afbd"
+                  />
+                  <div className="flex-grow">
+                    <h2 className="text-gray-900 title-font font-medium">
+                      Russell Weas
+                    </h2>
+                    <p className="text-gray-500">Backend Developer</p>
+                  </div>
+                </div>
+              </div>
+              <div className="p-2 lg:w-1/2 md:w-1/2 w-full">
+                <div className="h-full flex items-center border-gray-200 border p-4 rounded-lg">
+                  <img
+                    alt="team"
+                    className="w-16 h-16 bg-gray-100 object-cover object-center flex-shrink-0 rounded-full mr-4"
+                    src="https://dummyimage.com/84x84/edf2f7/a5afbd"
+                  />
+                  <div className="flex-grow">
+                    <h2 className="text-gray-900 title-font font-medium">
+                      Jonathan Pence
+                    </h2>
+                    <p className="text-gray-500">Backend Developer</p>
+                  </div>
+                </div>
+              </div>
+              <div className="p-2 lg:w-1/2 md:w-1/2 w-full">
+                <div className="h-full flex items-center border-gray-200 border p-4 rounded-lg">
+                  <img
+                    alt="team"
+                    className="w-16 h-16 bg-gray-100 object-cover object-center flex-shrink-0 rounded-full mr-4"
+                    src="https://dummyimage.com/88x88/edf2f7/a5afbd"
+                  />
+                  <div className="flex-grow">
+                    <h2 className="text-gray-900 title-font font-medium">
+                      Do Thien An Duong
+                    </h2>
+                    <p className="text-gray-500">Frontend Developer</p>
+                  </div>
+                </div>
+              </div>
+              <div className="p-2 lg:w-1/2 md:w-1/2 w-full">
+                <div className="h-full flex items-center border-gray-200 border p-4 rounded-lg">
+                  <img
+                    alt="team"
+                    className="w-16 h-16 bg-gray-100 object-cover object-center flex-shrink-0 rounded-full mr-4"
+                    src="https://dummyimage.com/90x90/edf2f7/a5afbd"
+                  />
+                  <div className="flex-grow">
+                    <h2 className="text-gray-900 title-font font-medium">
+                      Sanghyun Simon Jeon
+                    </h2>
+                    <p className="text-gray-500">Frontend Developer</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <NavLink
+              to="/About"
+              className="mt-3 text-nord7 font-medium inline-flex items-center"
+            >
+              Learn More
+              <svg
+                fill="none"
+                stroke="currentColor"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                className="w-4 h-4 ml-2"
+                viewBox="0 0 24 24"
+              >
+                <path d="M5 12h14M12 5l7 7-7 7"></path>
+              </svg>
+            </NavLink>
+          </div>
+        </section>
+        {/* <section className="text-gray-700 body-font border-t border-gray-200">
         <div className="container px-5 py-24 mx-auto">
           <div className="xl:w-1/2 lg:w-3/4 w-full mx-auto text-center">
             <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" className="inline-block w-8 h-8 text-gray-400 mb-8" viewBox="0 0 975.036 975.036">
@@ -99,7 +146,7 @@ export default class LandingPage extends React.Component{
           </div>
         </div>
       </section> */}
-      {/* <section>
+        {/* <section>
       <div className="flex flex-col min-h-screen justify-center items-center -mt-40">
         <div className="flex flex-col justify-center items-center">
           <h1 className="flex m-0 text-center leading-5 text-6xl">
@@ -151,7 +198,7 @@ export default class LandingPage extends React.Component{
         </div>
       </div>
     </section> */}
-    </>
-  );
+      </>
+    );
   }
 }
