60dd25531ff714094814da4ec846e3e0d5878d29
Simon Jeon <shjeon@crimson.ua.edu>
2021-10-06 20:29:54 -0500

    bg change

--- src/components/LandingPage/LandingPage.tsx
+++ src/components/LandingPage/LandingPage.tsx
@@ -11,7 +11,7 @@ export default class LandingPage extends React.Component {
   render() {
     return (
       <>
-        <section className="text-gray-700 body-font">
+        <section className="text-gray-700 body-font bg-nord6">
           <div className="container mx-auto flex px-5 py-24 md:flex-row flex-col items-center">
             <div className="flex-grow lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
               <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900">
